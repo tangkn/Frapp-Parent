4b23fe0d068abe3bec73f544fcc1db6268851b13
GokhanTAN <gokhandogutan@gmail.com>
2023-08-30 12:17:42 +0300

    Add security annotation

--- Frapp-Core/src/main/java/frapp/parent/FrappApplication.java
+++ Frapp-Core/src/main/java/frapp/parent/FrappApplication.java
@@ -3,8 +3,10 @@ package frapp.parent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class FrappApplication {
     public static void main(String[] args) {
         SpringApplication.run(FrappApplication.class, args);
